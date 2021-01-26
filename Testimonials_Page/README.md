d1b8e0c9b9e2ab25758ebf860d86f77e58a1bab1
Jyotsna555 <pjyotsna555@gmail.com>
2021-01-26 13:33:18 +0530

    Testimonials Page
    Submitted by Ayushee, Devika and Jyotsna

---
+++ Testimonials_Page/README.md
@@ -0,0 +1,1 @@
+# IEEE-WebD-Tetsimonials
---
+++ Testimonials_Page/java_d.js
@@ -0,0 +1,32 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
